9306f89128b79abd3fb4803be23fec7910749305
akash-1180 <80939273+akash-1180@users.noreply.github.com>
2021-06-04 15:42:28 +1000

    Add files via upload

---
+++ src/Run.java
@@ -0,0 +1,11 @@
+import mazerunner.engine.GameEngine;
+
+public class Run {
+
+
+
+    public static void main(String a[]) {
+        GameEngine gameEngine = new GameEngine();
+        gameEngine.startGame();
+    }
+}
